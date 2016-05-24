c3c734188216299f1eb897212e64b89c3c850c1c
Josip Ledic <josip.ledic@outlook.com>
2016-05-25 00:15:36 +0200

    changed port

--- Server.js
+++ Server.js
@@ -8,7 +8,7 @@ var io = require('socket.io').listen(server);
 users = [];
 connections = [];
 
-server.listen(3000);
+server.listen(process.env.PORT || 3000);
 console.log('Server running...');
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
